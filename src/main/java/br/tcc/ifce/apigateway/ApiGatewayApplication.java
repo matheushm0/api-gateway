183a2ba8adb9c8b810a7b7355a3d3f2b15a9b743
Matheus Holanda Matos <matheus.holandam@gmail.com>
2022-06-12 18:51:40 -0300

    Initializing api gateway

---
+++ src/main/java/br/tcc/ifce/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package br.tcc.ifce.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
